1454136be6b226b58048bf8b9821298468e3ea59
JoaoFrancisc0 <joaofrancisco1015@gmail.com>
2024-06-26 19:06:12 -0300

    Aula 1.3

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express'
 
+const app = express()
+
+// Mock <-- Lista para testar funções
+const listaJogos = [
+    {id: 1, nome: "Marvel's Spider-Man", trofeus: 74},
+    {id: 2, nome: "God of War", trofeus: 37},
+    {id: 3, nome: "Ratchet & Clank", trofeus: 47},
+    {id: 4, nome: "The Last of Us Remastered", trofeus: 50},
+    {id: 5, nome: "Horizon Zero Dawn", trofeus: 79}
+]
+
+// Criar rota padrão
 app.get('/', (req, res) => {
     res.send('Ola Mundo!')
 })
 
-app.listen(port, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+app.get('/games', (req, res) => {
+    res.status(200).send(listaJogos)
 })
+
+export default app
