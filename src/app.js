1daf15b2f877c3246646e928662e77b7eb99cd92
JoaoFrancisc0 <joaofrancisco1015@gmail.com>
2024-06-27 17:07:27 -0300

    Aula 1.5

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 // Mock <-- Lista para testar funções
-const listaJogos = [
+const listaGames = [
     {id: 1, nome: "Marvel's Spider-Man", trofeus: 74},
     {id: 2, nome: "God of War", trofeus: 37},
     {id: 3, nome: "Ratchet & Clank", trofeus: 47},
@@ -14,18 +14,38 @@ const listaJogos = [
     {id: 5, nome: "Horizon Zero Dawn", trofeus: 79}
 ]
 
+// Função de busca por id
+function buscarGamePorId(id) {
+    return listaGames.filter( game => game.id == id)
+}
+
+// Função de busca da posição na lista
+function buscarIndexGame(id) {
+    return listaGames.findIndex( game => game.id == id )
+}
+
 // Criar rota padrão
 app.get('/', (req, res) => {
     res.send('Ola Mundo!')
 })
 
 app.get('/games', (req, res) => {
-    res.status(200).send(listaJogos)
+    res.status(200).send(listaGames)
 })
 
 app.post('/games', (req, res) => {
-    listaJogos.push(req.body)
+    listaGames.push(req.body)
     res.status(201).send('Game adicionado com sucesso!')
+})
+
+app.get('/games/:id', (req, res) => {
+    res.json(buscarGamePorId(req.params.id))
+})
+
+app.delete('/games/:id', (req, res) => {
+    let index = buscarIndexGame(req.params.id)
+    listaGames.splice(index, 1)
+    res.send(`Game com id ${req.params.id} excluído com sucesso!`)
 })
 
 export default app
