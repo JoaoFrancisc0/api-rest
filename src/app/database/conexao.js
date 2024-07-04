2adb9bf79de014fbc30230e65f9c8a090ee28724
JoaoFrancisc0 <joaofrancisco1015@gmail.com>
2024-07-04 19:18:13 -0300

    Aula 1.12

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -10,4 +10,15 @@ const conexao = mysql.createConnection({
 
 conexao.connect()
 
+export const consulta = (sql, valores='', mensagemReject) => {
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, valores, (error, result) => {
+            if(error) return reject(mensagemReject)
+            // Convertendo o JSON em texto
+            const row = JSON.parse(JSON.stringify(result))
+            return resolve(row)
+        })  
+    })
+}
+
 export default conexao
--- src/app/repositories/GameRepository.js
+++ src/app/repositories/GameRepository.js
@@ -1,61 +1,30 @@
-import conexao from "../database/conexao.js"
+import { consulta } from "../database/conexao.js"
 
 class GameRepository {
     // Tive que usar o Promise pq o dizia que dava problema com o res
     create(game) {
         const sql = "INSERT INTO games SET ?"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, game, (error, result) => {
-                if(error) return reject("Não foi possível cadastrar")
-                // Convertendo o JSON em texto
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-            })  
-        })
+        return consulta(sql, game, "Não foi possível cadastrar")
     }
     
     findAll() {
         const sql = "SELECT * FROM games;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, (error, result) => {
-                if(error) return reject("Não foi possível localizar")
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, "Não foi possível encontrar")
     }
 
     findById(id) {
         const sql = "SELECT * FROM games WHERE id=?;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id, (error, result) => {
-                if(error) return reject("Não foi possível localizar")
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, id, "Não foi possível encontrar")
     }
 
     update(game, id) {
         const sql = "UPDATE games SET ? WHERE id=?;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, [game, id], (error, result) => {
-                if(error) return reject("Não foi possível atualizar")
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, [game, id], "Não foi possível atualizar")
     }
 
     delete(id) {
         const sql = "DELETE FROM games WHERE id=?;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id, (error, result) => {
-                if(error) return reject("Não foi possível deletar")
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, id, "Não foi possível remover")
     }
 }
 
